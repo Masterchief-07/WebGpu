87fdb26a800563d12f255c0466095d5e7b222092
Masterchief07 <kjonathan1999@hotmail.com>
2026-02-21 21:09:32 +0300

    create a simple instance

--- chaps/chap1_gettingStarted/main.cpp
+++ chaps/chap1_gettingStarted/main.cpp
@@ -1,8 +1,20 @@
+#include <webgpu/webgpu.h>
 #include <iostream>
 
 auto main() -> int{
 
-    std::cout<<"hello world\n";
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain = nullptr;
 
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+
+    if(!instance)
+    {
+        std::cerr<<"Could not initialize WebGPU\n";
+        return 1;
+    }
+
+    std::cout<<"WGPU instance: "<<instance<<"\n";
+    wgpuInstanceRelease(instance);
     return 0;
 }
